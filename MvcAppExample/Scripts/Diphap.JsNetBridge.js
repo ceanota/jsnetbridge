efa5d1b819c49d63a1fa30e5b1c15bbe0ef3dfc2
ceanota <tran-alexandre@hotmail.fr>
2015-12-26 08:13:41 +0100

    Changement de nom de l'application

--- MvcAppExample/Scripts/Diphap.JsNetBridge.js
+++ MvcAppExample/Scripts/Diphap.JsNetBridge.js
@@ -8,7 +8,7 @@
             
             Creator:    TRAN Alexandre 
                         tran-alexandre@hotmail.fr
-            Created:    26/12/2015 08:06:25, 01:00:00, Paris, Madrid (heure d’été)
+            Created:    26/12/2015 08:11:48, 01:00:00, Paris, Madrid (heure d’été)
 *******************************************************************************************/
 window.$dp = window.$dp || {};window.$dp.shared = window.$dp.shared || {};
 $dp.shared.arrayFactory=$dp.shared.arrayFactory||function (ref) {
