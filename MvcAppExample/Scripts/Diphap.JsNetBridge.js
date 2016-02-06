9d74f19dc29de4ed12106af62d12c5461111d0cb
ceanota <tran-alexandre@hotmail.fr>
2016-02-06 19:39:14 +0100

    New sln

--- MvcAppExample/Scripts/Diphap.JsNetBridge.js
+++ MvcAppExample/Scripts/Diphap.JsNetBridge.js
@@ -51,29 +51,98 @@
 //#endregion
 
 
-window.$dp = window.$dp || {};window.$dp.shared = window.$dp.shared || {};
-$dp.shared.arrayFactory=$dp.shared.arrayFactory||function (ref) {
-    var aa = [];
-    aa.$dpItemFactory = function () {
-        var result;
-        if(typeof ref === 'function'){ result = ref(); }
-        else { result = ref; }
-        return result;
+(function () {
+
+    window.$dp = window.$dp || {};
+    window.$dp.shared = window.$dp.shared || {};
+
+    if (window.$dp.shared.arrayFactory !== undefined) { return; }
+
+    $dp.shared.arrayFactory = function arrayFactory(ref) {
+        /// <signature>
+        ///   <summary>this function returns a array with added function '$dpItemFactory' who creates instance of item of array.</summary>
+        ///   <param name="ref" type="Function">ref is factory who creates instance of item of array</param>
+        ///   <returns type="Function" />
+        /// </signature>
+
+        var aa = [];
+        aa.$dpItemFactory = function () {
+            var result;
+            if (typeof ref === 'function') { result = ref(); }
+            else { result = ref; }
+            return result;
+        };
+        return aa;
     };
-    return aa;
-};
+
+})();
+
+(function () {
+    window.$dp = window.$dp || {};
+    window.$dp.shared = window.$dp.shared || {};
+
+    if (window.$dp.shared.circularReferenceManagerFactory !== undefined) { return; }
+
+    window.$dp.shared.circularReferenceManagerFactory = function circularReferenceManagerFactory(sameIntance) {
+        /// <signature>
+        ///   <summary>Factory gives un new instance of Function that handles the circular reference objects.</summary>
+        ///   <param name="sameIntance" type="bool">If sameInstance === undefined: new instance of function. Otherwise, we use the same instance.</param>
+        ///   <returns type="Function" />
+        /// </signature>
+        if (sameIntance === undefined) {
+            var newFunc = window.$dp.shared.circularReferenceManagerFactory._managerFunc.bind();
+            newFunc.factories = [];
+            window.$dp.shared.circularReferenceManagerFactory.instance = newFunc;
+        }
+        return window.$dp.shared.circularReferenceManagerFactory.instance;
+    }
+
+    window.$dp.shared.circularReferenceManagerFactory._managerFunc = function _managerFunc(func) {
+        /// <signature>
+        ///   <summary>it is the function that handles the circular reference objects</summary>
+        ///   <param name="func" type="Function">Factory of instances.</param>
+        ///   <returns type="Object" />
+        /// </signature>
+
+        var internalFunc = window.$dp.shared.circularReferenceManagerFactory.instance;
+
+        var foundedIdx = -1;
+        for (var idx = 0; idx < internalFunc.factories.length; idx++) {
+            if (internalFunc.factories[idx].item === func) {
+                foundedIdx = idx;
+            }
+        }
+
+        if (foundedIdx === -1) {
+            internalFunc.factories.push({ "item": func, "number": 0 });
+            foundedIdx = internalFunc.factories.length - 1;
+        } else {
+            internalFunc.factories[foundedIdx].number++;
+        }
+
+        if (internalFunc.factories[foundedIdx].number < 1) {
+            return internalFunc.factories[foundedIdx].item(true);
+        } else {
+            internalFunc.factories[foundedIdx].number = null;
+            return internalFunc.factories[foundedIdx].item;
+        }
+    }
+
+})();
+
+
 
 window.$dp = window.$dp || {};
 window.$dp.$JsNet = window.$dp.$JsNet || {};
 window.$dp.$JsNet.MvcAppExample = window.$dp.$JsNet.MvcAppExample || {};
 window.$dp.$JsNet.MvcAppExample.Models = window.$dp.$JsNet.MvcAppExample.Models || {};
-$dp.$JsNet.MvcAppExample.Models.UserProfile = $dp.$JsNet.MvcAppExample.Models.UserProfile || function(){ var obj = {"UserId":0,"UserName":""};obj.constructor=$dp.$JsNet.MvcAppExample.Models.UserProfile; return obj; };
-$dp.$JsNet.MvcAppExample.Models.RegisterExternalLoginModel = $dp.$JsNet.MvcAppExample.Models.RegisterExternalLoginModel || function(){ var obj = {"UserName":"","ExternalLoginData":""};obj.constructor=$dp.$JsNet.MvcAppExample.Models.RegisterExternalLoginModel; return obj; };
-$dp.$JsNet.MvcAppExample.Models.LocalPasswordModel = $dp.$JsNet.MvcAppExample.Models.LocalPasswordModel || function(){ var obj = {"OldPassword":"","NewPassword":"","ConfirmPassword":""};obj.constructor=$dp.$JsNet.MvcAppExample.Models.LocalPasswordModel; return obj; };
-$dp.$JsNet.MvcAppExample.Models.LoginModel = $dp.$JsNet.MvcAppExample.Models.LoginModel || function(){ var obj = {"UserName":"","Password":"","RememberMe":false};obj.constructor=$dp.$JsNet.MvcAppExample.Models.LoginModel; return obj; };
-$dp.$JsNet.MvcAppExample.Models.RegisterModel = $dp.$JsNet.MvcAppExample.Models.RegisterModel || function(){ var obj = {"UserName":"","Password":"","ConfirmPassword":""};obj.constructor=$dp.$JsNet.MvcAppExample.Models.RegisterModel; return obj; };
-$dp.$JsNet.MvcAppExample.Models.ExternalLogin = $dp.$JsNet.MvcAppExample.Models.ExternalLogin || function(){ var obj = {"Provider":"","ProviderDisplayName":"","ProviderUserId":""};obj.constructor=$dp.$JsNet.MvcAppExample.Models.ExternalLogin; return obj; };
+$dp.$JsNet.MvcAppExample.Models.UserProfile = $dp.$JsNet.MvcAppExample.Models.UserProfile || function(){ var args = Array.prototype.slice.call(arguments); var obj = {"UserId":0,"UserName":""};obj.constructor=$dp.$JsNet.MvcAppExample.Models.UserProfile; return obj; };
+$dp.$JsNet.MvcAppExample.Models.RegisterExternalLoginModel = $dp.$JsNet.MvcAppExample.Models.RegisterExternalLoginModel || function(){ var args = Array.prototype.slice.call(arguments); var obj = {"UserName":"","ExternalLoginData":""};obj.constructor=$dp.$JsNet.MvcAppExample.Models.RegisterExternalLoginModel; return obj; };
+$dp.$JsNet.MvcAppExample.Models.LocalPasswordModel = $dp.$JsNet.MvcAppExample.Models.LocalPasswordModel || function(){ var args = Array.prototype.slice.call(arguments); var obj = {"OldPassword":"","NewPassword":"","ConfirmPassword":""};obj.constructor=$dp.$JsNet.MvcAppExample.Models.LocalPasswordModel; return obj; };
+$dp.$JsNet.MvcAppExample.Models.LoginModel = $dp.$JsNet.MvcAppExample.Models.LoginModel || function(){ var args = Array.prototype.slice.call(arguments); var obj = {"UserName":"","Password":"","RememberMe":false};obj.constructor=$dp.$JsNet.MvcAppExample.Models.LoginModel; return obj; };
+$dp.$JsNet.MvcAppExample.Models.RegisterModel = $dp.$JsNet.MvcAppExample.Models.RegisterModel || function(){ var args = Array.prototype.slice.call(arguments); var obj = {"UserName":"","Password":"","ConfirmPassword":""};obj.constructor=$dp.$JsNet.MvcAppExample.Models.RegisterModel; return obj; };
+$dp.$JsNet.MvcAppExample.Models.ExternalLogin = $dp.$JsNet.MvcAppExample.Models.ExternalLogin || function(){ var args = Array.prototype.slice.call(arguments); var obj = {"Provider":"","ProviderDisplayName":"","ProviderUserId":""};obj.constructor=$dp.$JsNet.MvcAppExample.Models.ExternalLogin; return obj; };
 
 window.$dp = window.$dp || {};window.$dp.$JsNet = window.$dp.$JsNet || {};
-$dp.$JsNet.$UrlSet = {"Account":{"Login": {Url:null,Params:function(){ var obj = {"returnUrl":""}; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"Login": {Url:null,Params:function(){ var obj = {"model":$dp.$JsNet.MvcAppExample.Models.LoginModel(),"returnUrl":""}; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"LogOff": {Url:null,Params:function(){ var obj = null; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"Register": {Url:null,Params:function(){ var obj = null; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"Register": {Url:null,Params:function(){ var obj = {"model":$dp.$JsNet.MvcAppExample.Models.RegisterModel()}; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"Disassociate": {Url:null,Params:function(){ var obj = {"provider":"","providerUserId":""}; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"Manage": {Url:null,Params:function(){ var obj = {"message":0}; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"Manage": {Url:null,Params:function(){ var obj = {"model":$dp.$JsNet.MvcAppExample.Models.LocalPasswordModel()}; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"ExternalLogin": {Url:null,Params:function(){ var obj = {"provider":"","returnUrl":""}; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"ExternalLoginCallback": {Url:null,Params:function(){ var obj = {"returnUrl":""}; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"ExternalLoginConfirmation": {Url:null,Params:function(){ var obj = {"model":$dp.$JsNet.MvcAppExample.Models.RegisterExternalLoginModel(),"returnUrl":""}; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"ExternalLoginFailure": {Url:null,Params:function(){ var obj = null; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"ExternalLoginsList": {Url:null,Params:function(){ var obj = {"returnUrl":""}; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"RemoveExternalLogins": {Url:null,Params:function(){ var obj = null; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }}},
-"Home":{"Index": {Url:null,Params:function(){ var obj = null; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"About": {Url:null,Params:function(){ var obj = null; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }},"Contact": {Url:null,Params:function(){ var obj = null; return obj; },Return:function(){ var obj = {}; return obj; },IsApiController:null,AjaxOptions:function(){ var obj = {}; return obj; }}}};
+$dp.$JsNet.$UrlSet = {"Account":{"Login": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = {"returnUrl":""}; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();action.sig1 = function actionFactory () { var action = {};action.Params = function(){  var obj = {"model":$dp.$JsNet.MvcAppExample.Models.LoginModel(),"returnUrl":""}; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }(),"LogOff": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = null; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }(),"Register": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = null; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();action.sig1 = function actionFactory () { var action = {};action.Params = function(){  var obj = {"model":$dp.$JsNet.MvcAppExample.Models.RegisterModel()}; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }(),"Disassociate": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = {"provider":"","providerUserId":""}; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }(),"Manage": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = {"message":0}; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();action.sig1 = function actionFactory () { var action = {};action.Params = function(){  var obj = {"model":$dp.$JsNet.MvcAppExample.Models.LocalPasswordModel()}; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }(),"ExternalLogin": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = {"provider":"","returnUrl":""}; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }(),"ExternalLoginCallback": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = {"returnUrl":""}; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }(),"ExternalLoginConfirmation": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = {"model":$dp.$JsNet.MvcAppExample.Models.RegisterExternalLoginModel(),"returnUrl":""}; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }(),"ExternalLoginFailure": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = null; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }(),"ExternalLoginsList": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = {"returnUrl":""}; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }(),"RemoveExternalLogins": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = null; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }()},
+"Home":{"Index": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = null; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }(),"About": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = null; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }(),"Contact": function actionFactory () { var action = {};action.Url = null;action.sig0 = function actionFactory () { var action = {};action.Params = function(){  var obj = null; return obj; };action.Return = function(){  var obj = {}; return obj; };action.IsApiController = null;action.AjaxOptions = function(){  var obj = {}; return obj; };return action; }();return action; }()}};
