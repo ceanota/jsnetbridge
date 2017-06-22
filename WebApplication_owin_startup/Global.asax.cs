c4c6381c3461ac37056b13ecf8f4fcf6f43bcc57
ceanota <tran-alexandre@hotmail.fr>
2017-06-22 17:49:54 +0200

    bug application

---
+++ WebApplication_owin_startup/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+        }
+
+        #region "JsNetBridge: it will help generate the url in the layout of views."
+        static Diphap.JsNetBridge.Mvc.AspMvcInfo _AspMvcInfo;
+        static public Diphap.JsNetBridge.Mvc.AspMvcInfo AspMvcInfo
+        {
+            get
+            {
+                if (_AspMvcInfo == null)
+                {
+                    _AspMvcInfo = new Diphap.JsNetBridge.Mvc.AspMvcInfo(System.Reflection.Assembly.GetExecutingAssembly());
+                }
+                return _AspMvcInfo;
+            }
+        }
+        #endregion
+
+    }
+}
